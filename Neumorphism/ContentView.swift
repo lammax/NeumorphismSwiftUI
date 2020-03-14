4f966f96a174a25ab21fc955090ce3139fac93cd
Lammax <lammax@mail.ru>
2020-03-14 13:18:14 +0300

    Button inner gradient shadows

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -14,6 +14,14 @@ extension Color {
     
 }
 
+extension LinearGradient {
+    
+    init(_ colors: Color...) {
+        self.init(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing)
+    }
+    
+}
+
 struct SimpleButtonStyle: ButtonStyle {
     func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
@@ -24,8 +32,20 @@ struct SimpleButtonStyle: ButtonStyle {
                 if configuration.isPressed {
                     Circle()
                     .fill(Color.offWhite)
-                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: -5, y: -5)
-                    .shadow(color: Color.white.opacity(0.7), radius: 10, x: 10, y: 10)
+                    .overlay(
+                        Circle()
+                            .stroke(Color.gray, lineWidth: 4)
+                            .blur(radius: 4)
+                            .offset(x: 2, y: 2)
+                            .mask(Circle().fill(LinearGradient(Color.black, Color.clear)))
+                    )
+                    .overlay(
+                        Circle()
+                            .stroke(Color.white, lineWidth: 8)
+                            .blur(radius: 4)
+                            .offset(x: -2, y: -2)
+                            .mask(Circle().fill(LinearGradient(Color.clear, Color.black)))
+                    )
                 } else {
                     Circle()
                     .fill(Color.offWhite)
