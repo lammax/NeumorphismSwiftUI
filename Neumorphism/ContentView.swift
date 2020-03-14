40b1d46ea238996d9c5f1da10731077c1b662b5f
Lammax <lammax@mail.ru>
2020-03-14 12:37:34 +0300

    Heart button

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -14,17 +14,24 @@ extension Color {
     
 }
 
+struct SimpleButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+    }
+}
 
 struct ContentView: View {
     var body: some View {
         ZStack {
             Color.offWhite
             
-            RoundedRectangle(cornerRadius: 25)
-            .fill(Color.offWhite)
-            .frame(width: 300, height: 300)
-            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
-            .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+            Button(action: {
+                print("button tapped")
+            }) {
+                Image(systemName: "heart.fill")
+                    .foregroundColor(.gray)
+            }
+        .buttonStyle(SimpleButtonStyle())
         }
         .edgesIgnoringSafeArea(.all)
     }
