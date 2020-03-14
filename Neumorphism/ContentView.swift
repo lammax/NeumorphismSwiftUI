af9492066483f8971074e748844eb0708afaac1f
Lammax <lammax@mail.ru>
2020-03-14 12:47:27 +0300

    Catch tap action around all the Circle shape

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -18,11 +18,19 @@ struct SimpleButtonStyle: ButtonStyle {
     func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
         .padding(30)
+        .contentShape(Circle())
         .background(
-            Circle()
-            .fill(Color.offWhite)
-            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
-            .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+            Group {
+                if configuration.isPressed {
+                    Circle()
+                    .fill(Color.offWhite)
+                } else {
+                    Circle()
+                    .fill(Color.offWhite)
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                    .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                }
+            }
         )
     }
 }
