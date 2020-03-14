9b3af540beb53d06581fae004e8d1da47b386d97
Lammax <lammax@mail.ru>
2020-03-14 12:40:47 +0300

    Rounded button with shadows

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -17,6 +17,13 @@ extension Color {
 struct SimpleButtonStyle: ButtonStyle {
     func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
+        .padding(30)
+        .background(
+            Circle()
+            .fill(Color.offWhite)
+            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+            .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+        )
     }
 }
 
