efd483123cea2b61ccaeb2b1d6554644e7abc0e2
Lammax <lammax@mail.ru>
2020-03-14 14:05:29 +0300

    Dark button gradient stroke

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -68,11 +68,13 @@ struct DarkBackground<S: Shape>: View {
             if isHighlighted {
                 shape
                     .fill(LinearGradient(Color.darkEnd, Color.darkStart))
+                    .overlay(shape.stroke(LinearGradient(Color.darkStart, Color.darkEnd), lineWidth: 4))
                     .shadow(color: Color.darkStart, radius: 10, x: 5, y: 5)
                     .shadow(color: Color.darkEnd, radius: 10, x: -5, y: -5)
             } else {
                 shape
                     .fill(LinearGradient(Color.darkStart, Color.darkEnd))
+                    .overlay(shape.stroke(LinearGradient(Color.darkEnd, Color.darkStart), lineWidth: 4))
                     .shadow(color: Color.darkStart, radius: 10, x: -10, y: -10)
                     .shadow(color: Color.darkEnd, radius: 10, x: 10, y: 10)
             }
