8be83179f025e48cdbcbb4ca1cc2090d2c3e2996
Lammax <lammax@mail.ru>
2020-03-14 13:44:20 +0300

    Black gradient background

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -12,6 +12,8 @@ extension Color {
     
     static let offWhite = Color(red: 225 / 255, green: 225 / 255, blue: 235 / 255)
     
+    static let darkStart = Color(red: 50 / 255, green: 60 / 255, blue: 65 / 255)
+    static let darkEnd = Color(red: 25 / 255, green: 25 / 255, blue: 30 / 255)
 }
 
 extension LinearGradient {
@@ -60,7 +62,7 @@ struct SimpleButtonStyle: ButtonStyle {
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color.offWhite
+            LinearGradient(Color.darkStart, Color.darkEnd)
             
             Button(action: {
                 print("button tapped")
