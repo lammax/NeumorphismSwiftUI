64a882186c6581ebc07096e32c86046b64051adb
Lammax <lammax@mail.ru>
2020-03-14 12:50:09 +0300

    Button reverse shadows

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -24,6 +24,8 @@ struct SimpleButtonStyle: ButtonStyle {
                 if configuration.isPressed {
                     Circle()
                     .fill(Color.offWhite)
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: -5, y: -5)
+                    .shadow(color: Color.white.opacity(0.7), radius: 10, x: 10, y: 10)
                 } else {
                     Circle()
                     .fill(Color.offWhite)
