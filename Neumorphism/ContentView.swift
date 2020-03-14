86af9910c9f858d0b9bab5cce9eb59619be14ff7
Lammax <lammax@mail.ru>
2020-03-14 12:31:18 +0300

    White shadow to the upper left

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -24,6 +24,7 @@ struct ContentView: View {
             .fill(Color.offWhite)
             .frame(width: 300, height: 300)
             .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+            .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
         }
         .edgesIgnoringSafeArea(.all)
     }
